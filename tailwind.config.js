aff94dd69952fa788e496c64ae06d3265ce77082
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-28 20:38:38 +0530

    Added color from frontend mentor style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
         Fraunces: ["Fraunces", "serif"],
         Montserrat: ["Montserrat", "sans-serif"],
       },
+      colors: {
+        fmLightGreen: "#3c8067",
+        fmDarkGreen: "#1a4031",
+        fmCream: "#f2ebe3",
+        fmDarkBlue: "#1c232b",
+        fmTextColor: "#6c7289",
+      },
     },
   },
   plugins: [],
