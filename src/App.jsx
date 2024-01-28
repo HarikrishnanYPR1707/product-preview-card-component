b02247e374f607f3f55dfe412ed200a22dfcfb44
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-28 20:43:30 +0530

    imported component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProductPreviewCard from "./components/ProductPreviewCard";
 
 const App = () => {
-  return <section className="bg-fmCream h-screen p-5"></section>;
+  return (
+    <section className="bg-fmCream h-screen p-5">
+      <ProductPreviewCard />
+    </section>
+  );
 };
 
 export default App;
