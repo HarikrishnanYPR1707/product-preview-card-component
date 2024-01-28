3ccf30c07fdcc7641eeeeb1080316258da4fcfa2
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-28 18:28:35 +0530

    Added frontendmentor files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      Perfume Gabrielle Essence Eau De Parfum A floral, solar and voluptuous
+      interpretation composed by Olivier Polge, Perfumer-Creator for the House
+      of CHANEL. $149.99 $169.99 Add to Cart
+    </div>
+  );
+};
 
-export default App
+export default App;
